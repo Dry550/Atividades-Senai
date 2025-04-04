6f960c0a70456435214f945d5870103e53799965
Dry <adryelle.souza6@aluno.senai.br>
2025-04-04 16:53:22 -0300

    Aula 42 Intensidade led

---
+++ aula_42_intensidade_led1.ino
@@ -0,0 +1,153 @@
+/*
+Autor:Adryelle Souza
+Nome do arquivo:Intensidade led
+Data: 21/03/2025
+Descrição:  
+*/
+
+#include <LiquidCrystal_I2C.h>
+#define valorBotao1 0
+#define valorBotao2 145  
+#define valorBotao3 329  
+#define valorBotao4 505
+#define valorBotao5 741
+#define valorSolto 1023
+#define pinLedA 10
+#define pinLedB 11
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print(">LED A OFF");
+  lcd.setCursor(0,1);
+  lcd.print(" LED B OFF");
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+    int valorLeituraAnalogica = analogRead(A0);
+  
+    static int valorAnterior = 1023;
+    static bool posicao = 0;
+    static int intensidadeA = 0;
+    static int intensidadeB = 0;
+    static int atualizacao = 0;
+  //***** TRATAMENTO DOS BOTOES *******
+  // NENHUM BOTAO
+  if (valorLeituraAnalogica == valorSolto)
+   {
+     
+   }
+ 
+  //BOTAO 1 PRESSIONADO
+     else if(valorLeituraAnalogica == valorBotao1 &&
+             valorAnterior == 1023)
+    {
+       posicao = 0;
+
+    }
+ 
+   //BOTAO 2 PRESSIONADO
+      else if(valorLeituraAnalogica >= valorBotao2 * 0.9  &&
+            valorLeituraAnalogica <= valorBotao2 * 1.1 &&
+            valorAnterior == 1023)
+    {
+      posicao = 1;
+    }
+ 
+   //BOTAO 3 PRESSIONADO
+    else if(valorLeituraAnalogica >= valorBotao3 * 0.9  &&
+            valorLeituraAnalogica <= valorBotao3 * 1.1 &&
+            valorAnterior == 1023)
+    { 
+      atualizacao = 1;
+      if(posicao == 0)
+      {
+        if(intensidadeA > 0) 
+      	intensidadeA = intensidadeA - 10;
+        Serial.println(intensidadeA);
+        
+      }else
+      {
+        if(intensidadeB > 0) 
+      	intensidadeB = intensidadeB - 10;
+		Serial.println(intensidadeB);
+
+      }
+    }
+   
+   //BOTAO 4 PRESSIONADO
+   else if(valorLeituraAnalogica >= valorBotao4 * 0.9  &&
+            valorLeituraAnalogica <= valorBotao4 * 1.1 &&
+            valorAnterior == valorSolto)
+    {
+     atualizacao = 1;
+     if(posicao == 0)
+      {
+        if(intensidadeA <100) 
+      	intensidadeA = intensidadeA + 10;
+        Serial.println(intensidadeA);
+        
+      }else
+      {
+        if(intensidadeB < 100) 
+      	intensidadeB = intensidadeB + 10;
+		Serial.println(intensidadeB);
+
+      
+    }
+   
+    }
+ 
+   //BOTAO 5 PRESSIONADO
+      else if(valorLeituraAnalogica >= valorBotao5 * 0.9  &&
+            valorLeituraAnalogica <= valorBotao5 * 1.1 &&
+            valorAnterior == 1023)
+    {
+        if(posicao == 0){
+        	intensidadeA = !intensidadeA;
+    	}
+        else {intensidadeB = !intensidadeB;
+        
+    	}
+    }
+ 
+  valorAnterior = valorLeituraAnalogica;
+
+  //****** FIM DO TRATAMENTO DOS BOTOES ******
+ 
+ 
+  // ***** ATUALIZA DISPLAY ******
+  if(posicao == 0)
+  {
+    lcd.setCursor(0,0);
+    lcd.print(">");
+    lcd.setCursor(0,1);
+    lcd.print(" ");
+    lcd.setCursor(7, 0);
+    
+   
+  }
+  else
+  {
+    lcd.setCursor(0,1);
+    lcd.print(">");
+    lcd.setCursor(0,0);
+    lcd.print(" ");
+    lcd.setCursor(7, 1);
+      
+  
+  }
+ 
+  //***** ATUALIZACAO DOS LED ******
+
+  int resultado = map(25, 0, 255, 0, 100);
+  lcd.print(resultado);
+}
